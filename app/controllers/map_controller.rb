5a0ff71c3fed4cb2f9f3252b0d78867e04061493
Renaud Martinet <karouf@standrewslounge.net>
2008-04-08 21:42:16 +0200

    Modified Map controller so it gives location and markers for the map to display

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,2 +1,15 @@
 class MapController < ApplicationController
+
+	def index
+		@location = {:lon => 3.0906, :lat => 45.7694, :zoom => 13}
+		
+		@markers = [{:lon => 3.1173, :lat => 45.7935},
+						{:lon => 3.0852, :lat => 45.7784},
+						{:lon => 3.1022, :lat => 45.7623},
+						{:lon => 3.0758, :lat => 45.7444},
+						{:lon => 3.1123, :lat => 45.7693}
+					]
+					
+		
+	end
 end
