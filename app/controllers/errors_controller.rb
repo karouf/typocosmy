16db752508fa0bf52339284238171cce23f4ad9e
Renaud Martinet <karouf@standrewslounge.net>
2008-04-12 13:29:16 +0200

    Finished porting prototype map to OpenLayers 2.6

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@ class ErrorsController < ApplicationController
 
 	def index
 		@location = {:lon => 3.0906, :lat => 45.7694, :zoom => 13}
-		@markers = Error.find(:all, :order => "created_at DESC", :limit => 3)	
+		@errors = Error.find(:all, :order => "created_at DESC", :limit => 3)	
 	end
 
 end
