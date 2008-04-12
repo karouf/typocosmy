d8d34d2a7074a6205461cadacc776b89eef817be
Renaud Martinet <karouf@standrewslounge.net>
2008-04-12 22:44:05 +0200

    Modified OpenLayers controls so every panning or zooming of the map will update the markers

---
+++ public/openlayers/lib/Typocosmy.js
@@ -0,0 +1,12 @@
+function includeJavaScript(jsFile)
+{
+  document.write('<script type="text/javascript" src="'
+    + jsFile + '"></script>'); 
+}
+
+includeJavaScript('openlayers/lib/Typocosmy/Control/PanZoom.js');
+includeJavaScript('openlayers/lib/Typocosmy/Control/PanZoomBar.js');
+includeJavaScript('openlayers/lib/Typocosmy/Control/DragPan.js');
+includeJavaScript('openlayers/lib/Typocosmy/Control/ZoomBox.js');
+includeJavaScript('openlayers/lib/Typocosmy/Control/Navigation.js');
+
