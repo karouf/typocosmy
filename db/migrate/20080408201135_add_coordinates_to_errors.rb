48fa1ff5d28389c53a154ea1f361c36cac386274
Renaud Martinet <karouf@standrewslounge.net>
2008-04-17 21:13:41 +0200

    Modified to stick to conventions

--- db/migrate/20080408201135_add_coordinates_to_errors.rb
+++ db/migrate/20080408201135_add_coordinates_to_errors.rb
@@ -1,11 +1,11 @@
 class AddCoordinatesToErrors < ActiveRecord::Migration
   def self.up
-    add_column "errors", "lon", :decimal, :precision => 9, :scale => 6
-    add_column "errors", "lat", :decimal, :precision => 9, :scale => 6
+    add_column :errors, :lon, :decimal, :precision => 9, :scale => 6
+    add_column :errors, :lat, :decimal, :precision => 9, :scale => 6
   end
 
   def self.down
-    remove_column "errors", "lon"
-    remove_column "errors", "lat"
+    remove_column :errors, :lon
+    remove_column :errors, :lat
   end
 end
